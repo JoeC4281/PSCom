87c2d7d298f8c51020d1a8f038cfb1a610c86198
JoeC4281 <58880711+JoeC4281@users.noreply.github.com>
2025-04-23 09:27:49 -0400

    Add Intellisense
    via IPSScript Interface

--- PSRun.vb
+++ PSRun.vb
@@ -1,11 +1,21 @@
 ﻿Imports System.Management.Automation
 Imports System.Runtime.InteropServices
 
-<ComVisible(True)> ' Make class COM-visible
-<Guid("A5B64F74-0899-4475-853D-F81CB2024AE8")> ' Unique GUID for COM class
+<ComVisible(True)>
+<Guid("A1285800-644C-4497-9478-DB42AF3634D7")>
+Public Interface IPSScript
+    Function ExecuteScript(scriptPath As String) As String
+    Function ExecuteCommand(command As String) As String
+End Interface
+
+<ComVisible(True)>
+<Guid("A5B64F74-0899-4475-853D-F81CB2024AE8")>
 <ProgId("PSCom.PSScript")>
 Public Class PSScript
-    Public Function ExecuteScript(scriptPath As String) As String
+
+    Implements IPSScript
+
+    Public Function ExecuteScript(scriptPath As String) As String Implements IPSScript.ExecuteScript
         Try
             Using ps As PowerShell = PowerShell.Create()
                 ' Read the PowerShell script from the file
@@ -21,12 +31,13 @@ Public Class PSScript
         End Try
     End Function
 
-    Public Function ExecuteCommand(command As String) As String
+    Public Function ExecuteCommand(command As String) As String Implements IPSScript.ExecuteCommand
         Try
             Using ps As PowerShell = PowerShell.Create()
                 ' Add the script directly, not treating it as a file path
                 ps.AddScript(command)
                 Dim results = ps.Invoke()
+
                 Dim output As String = String.Join(Environment.NewLine, results.Select(Function(r) r.ToString()))
                 Return output
             End Using
